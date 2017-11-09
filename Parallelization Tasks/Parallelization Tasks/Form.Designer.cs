c1ee17220d90621ee3c208223e06a7f4a182d081
Alexander Usov <alexusov@photographer.net>
2017-11-09 18:16:49 +0300

    Practical_works

---
+++ Parallelization Tasks/Parallelization Tasks/Form.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Parallelization_Tasks
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.duration = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.radioButtonSingle = new System.Windows.Forms.RadioButton();
+            this.radioButtonMulti = new System.Windows.Forms.RadioButton();
+            this.label = new System.Windows.Forms.Label();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.textBoxPixelWidth = new System.Windows.Forms.TextBox();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.textBoxPixelHeight = new System.Windows.Forms.TextBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(483, 500);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 30);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // duration
+            // 
+            this.duration.AutoSize = true;
+            this.duration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.duration.Location = new System.Drawing.Point(12, 505);
+            this.duration.Name = "duration";
+            this.duration.Size = new System.Drawing.Size(92, 20);
+            this.duration.TabIndex = 1;
+            this.duration.Text = "Time (ms):";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.MaximumSize = new System.Drawing.Size(640, 480);
+            this.pictureBox.MinimumSize = new System.Drawing.Size(640, 480);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInfo.Location = new System.Drawing.Point(12, 540);
+            this.labelInfo.MaximumSize = new System.Drawing.Size(450, 20);
+            this.labelInfo.MinimumSize = new System.Drawing.Size(450, 20);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(450, 20);
+            this.labelInfo.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(577, 500);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 30);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "SAVE";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // radioButtonSingle
+            // 
+            this.radioButtonSingle.Checked = true;
+            this.radioButtonSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonSingle.Location = new System.Drawing.Point(475, 538);
+            this.radioButtonSingle.Name = "radioButtonSingle";
+            this.radioButtonSingle.Size = new System.Drawing.Size(126, 25);
+            this.radioButtonSingle.TabIndex = 0;
+            this.radioButtonSingle.TabStop = true;
+            this.radioButtonSingle.Text = "Single-Thread";
+            this.radioButtonSingle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMulti
+            // 
+            this.radioButtonMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonMulti.Location = new System.Drawing.Point(475, 578);
+            this.radioButtonMulti.Name = "radioButtonMulti";
+            this.radioButtonMulti.Size = new System.Drawing.Size(126, 25);
+            this.radioButtonMulti.TabIndex = 5;
+            this.radioButtonMulti.Text = "Multi-Thread";
+            this.radioButtonMulti.UseVisualStyleBackColor = true;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label.Location = new System.Drawing.Point(13, 585);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(94, 13);
+            this.label.TabIndex = 6;
+            this.label.Text = "Alexander Usov ©";
+            // 
+            // trackBar
+            // 
+            this.trackBar.LargeChange = 6;
+            this.trackBar.Location = new System.Drawing.Point(607, 536);
+            this.trackBar.Maximum = 24;
+            this.trackBar.MaximumSize = new System.Drawing.Size(45, 75);
+            this.trackBar.Minimum = 4;
+            this.trackBar.MinimumSize = new System.Drawing.Size(45, 75);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar.Size = new System.Drawing.Size(45, 75);
+            this.trackBar.TabIndex = 7;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar.Value = 5;
+            this.trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
+            // 
+            // textBoxPixelWidth
+            // 
+            this.textBoxPixelWidth.Location = new System.Drawing.Point(216, 585);
+            this.textBoxPixelWidth.MaxLength = 5;
+            this.textBoxPixelWidth.Name = "textBoxPixelWidth";
+            this.textBoxPixelWidth.Size = new System.Drawing.Size(60, 20);
+            this.textBoxPixelWidth.TabIndex = 8;
+            this.textBoxPixelWidth.Text = "10240";
+            this.textBoxPixelWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResolution.Location = new System.Drawing.Point(249, 565);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(75, 17);
+            this.labelResolution.TabIndex = 9;
+            this.labelResolution.Text = "Resolution";
+            // 
+            // textBoxPixelHeight
+            // 
+            this.textBoxPixelHeight.Location = new System.Drawing.Point(320, 585);
+            this.textBoxPixelHeight.MaxLength = 5;
+            this.textBoxPixelHeight.Name = "textBoxPixelHeight";
+            this.textBoxPixelHeight.Size = new System.Drawing.Size(60, 20);
+            this.textBoxPixelHeight.TabIndex = 10;
+            this.textBoxPixelHeight.Text = "7680";
+            this.textBoxPixelHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWidth.Location = new System.Drawing.Point(189, 586);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(21, 17);
+            this.labelWidth.TabIndex = 11;
+            this.labelWidth.Text = "W";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeight.Location = new System.Drawing.Point(296, 586);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(18, 17);
+            this.labelHeight.TabIndex = 12;
+            this.labelHeight.Text = "H";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 611);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.textBoxPixelHeight);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.textBoxPixelWidth);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.radioButtonMulti);
+            this.Controls.Add(this.radioButtonSingle);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.duration);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(680, 650);
+            this.MinimumSize = new System.Drawing.Size(680, 650);
+            this.Name = "Form";
+            this.Text = "Parallelization Tasks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label duration;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.RadioButton radioButtonSingle;
+        private System.Windows.Forms.RadioButton radioButtonMulti;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.TextBox textBoxPixelWidth;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.TextBox textBoxPixelHeight;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelHeight;
+    }
+}
+
---
+++ Parallelization Tasks/Parallelization Tasks/Form.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parallelization_Tasks
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form() { // constructor
+            InitializeComponent();
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void setPixels(TextBox textBoxPixelWidth, TextBox textBoxPixelHeight)
+        {
+            bool f = false;
+            foreach(char c in textBoxPixelWidth.Text)
+            {
+                if (Char.IsNumber(c)) f = true;
+                else { throw new Exception("Incorrect data!"); }
+            }
+            foreach (char c2 in textBoxPixelHeight.Text)
+            {
+                if (Char.IsNumber(c2)) f = true;
+                else { throw new Exception("Incorrect data!"); }
+            }
+            if (f == true)
+            {
+                this.pixelWidth = Int32.Parse(textBoxPixelWidth.Text);
+                this.pixelHeight = Int32.Parse(textBoxPixelHeight.Text);
+            }
+        }
+
+        // горизонтальное и вертикальное разрешение для Bitmap
+        private int pixelWidth = 10240, pixelHeight = 7680;
+        private int bytesPerPixel = 3; // 1 пиксельное значение в 3 байтах
+        private byte redValue,
+                     greenValue,
+                     blueValue;
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = null;
+            try
+            {
+                setPixels(textBoxPixelWidth, textBoxPixelHeight);
+
+                /// создать "пустой" (all-zeros) 24bpp Bitmap объект для вывода графики
+                using (Bitmap bmpRGB = new Bitmap(pixelWidth, pixelHeight, PixelFormat.Format24bppRgb))
+                {
+                    /// создать Rectangle и заблокировать растровое изображение в системной памяти
+                    Rectangle rect = new Rectangle(0, 0, pixelWidth, pixelHeight);
+                    BitmapData bmpData = bmpRGB.LockBits(rect, ImageLockMode.WriteOnly, bmpRGB.PixelFormat);
+
+                    // генерирует случайные значения для цветности RGB графики
+                    Random rand = new Random();
+                    redValue = (byte)rand.Next(0xFF);
+                    greenValue = (byte)rand.Next(0xFF);
+                    blueValue = (byte)rand.Next(0xFF);
+
+                    #region RUNNING IN FLOWS
+                    // запуск таймера
+                    Stopwatch watch = Stopwatch.StartNew(); // применяется для операций отсчета времени
+
+                    var tb = trackBar.Value;
+                    if (radioButtonMulti.Checked == true)
+                    {
+                        // данные для всего изображения находятся между 0 и pixelWidth / 2
+                        Task first = Task.Run(() => generateGraphData(ref bmpData, tb, 0, pixelWidth / 8)); // range is specified only for +X
+                        Task second = Task.Run(() => generateGraphData(ref bmpData, tb, pixelWidth / 8, pixelWidth / 4));
+                        Task third = Task.Run(() => generateGraphData(ref bmpData, tb, pixelWidth / 4, pixelWidth * 3 / 8));
+                        Task fourth = Task.Run(() => generateGraphData(ref bmpData, tb, pixelWidth * 3 / 8, pixelWidth / 2));
+                        Task.WaitAll(first, second, third, fourth); // task synchronization
+                    }
+                    if (radioButtonSingle.Checked == true)
+                    {
+                        generateGraphData(ref bmpData, tb, 0, pixelWidth / 2);
+                    }
+
+                    // отображение времени, затраченного на создание данных
+                    duration.Text = $"Time (ms): {watch.ElapsedMilliseconds}";
+                    #endregion
+
+                    #region displaying
+                    if (bmpData != null)
+                        // разблокировка растрового изображения из системной памяти
+                        bmpRGB.UnlockBits(bmpData);
+
+
+
+                    // вывести графику в pictureBox
+                    var memoryStream = new MemoryStream();
+                    bmpRGB.Save(memoryStream, ImageFormat.Bmp);
+                    Image image = Image.FromStream(memoryStream);
+                    pictureBox.Image = image;
+                    bmpRGB.Dispose();
+                    memoryStream.Dispose();
+                    #endregion
+                }
+            }
+            catch (Exception ex) { labelInfo.Text = ex.Message; }
+            finally { }
+        }
+
+        // генерирует данные для графики
+        private void generateGraphData(ref BitmapData bmpData, int tb, int partitionStart, int partitionEnd)
+        {
+            int Y = pixelHeight / 2, X = pixelWidth / 2;
+
+            for (short x = (short)partitionStart; x < partitionEnd; x++) // iterations = X
+            {
+                double p = Math.Sqrt(X * X - x * x);
+
+                for (var i = p; i > -p; i -= 5) // value X for -Y and Y
+                {
+                    double r = Math.Sqrt(x * x + i * i) / X;
+                    double q = (r - 1) * Math.Sin(tb * r);
+                    double y = i / 3 + (q * Y);
+
+                    #region plotXY
+                    // изображение зеркально относительно оси X поэтому метод plotXY вызывается дважды
+                    plotXY(ref bmpData, (int)(-x + (pixelWidth / 2)), (int)(y + (pixelHeight / 2)));
+                    plotXY(ref bmpData, (int)(x + (pixelWidth / 2)), (int)(y + (pixelHeight / 2)));
+                    #endregion
+                }
+            }
+        }
+
+        // устанавливает данные байтов для каждой точки,
+        // значения байтов соответствуют X и Y координатам
+        private void plotXY(ref BitmapData bmpData, int x, int y)
+        {
+            unsafe
+            {
+                // присвоить указателю адрес первых пиксельных данных
+                byte* ptr = (byte*)bmpData.Scan0;
+
+                // построение пиксела
+                int index = (y * pixelWidth + x ) * bytesPerPixel;
+                // назначить компонент RGB-значений к указателю
+                ptr[index + 2] = redValue;
+                ptr[index + 1] = greenValue;
+                ptr[index] = blueValue;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                Image image = pictureBox.Image;
+                image.Save("newImage.png", ImageFormat.Png);
+
+                labelInfo.Text = "This image was converted to PNG and saved!";
+            }
+            else
+                labelInfo.Text = "Necessary to build a image!";
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            labelInfo.Text = $"figure {(Math.Round(trackBar.Value / 1.0)).ToString()}";
+        }
+    }
+}
